82de1c876610c1f64d0b41c3513a8414a882f591
xingsensong <xingsensong@naradapower.com>
2024-07-04 17:10:06 +0800

    feat: 主题配置

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,9 +1,18 @@
 import { Outlet } from 'react-router-dom'
+import { Button } from 'antd-mobile'
 
 function Layout() {
   return (
     <div>
       <h3>我是Layout</h3>
+      {/* 测试全局生效样式 */}
+      <Button color='primary'>测试全局</Button>
+
+      {/* 测试局部生效样式 */}
+      {/* <div className='puple'>
+        <Button color='primary'>测试局部</Button>
+      </div> */}
+
       <Outlet></Outlet>
     </div>
   )
