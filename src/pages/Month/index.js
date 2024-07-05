63a7eff4edfd8af6f54e26a550cc5cd21b11fd90
xingsensong <xingsensong@naradapower.com>
2024-07-05 14:28:19 +0800

    feat: 月度-支出/收入/结余 合计计算

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -14,7 +14,6 @@ function Month() {
     // return出去计算后的值
     return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY-MM') )
   }, [billList])
-  console.log(monthGroup)
 
   // 控制时间弹窗的打开和关闭
   const [dateVisible, setDateVisible] = useState(false)
@@ -22,13 +21,28 @@ function Month() {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().format('YYYY-MM')
   })
-  // 时间切换
+
+  // 账单列表
+  const [currentMonthList, setCurrentMonthList] = useState([])
+  // 时间切换, 找到对应月份的数据
   const onDateConfirm = (date) => {
     setDateVisible(false)
     setCurrentDate(dayjs(date).format('YYYY-MM'))
+    console.log(monthGroup[currentDate])
+    setCurrentMonthList(monthGroup[currentDate])
   }
 
-  console.log(monthGroup[currentDate])
+  // 支出/收入/结余 合计计算
+  const moneyTotal = useMemo(() => {
+    const payTotal =  currentMonthList.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+    const incomeTotal = currentMonthList.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+    return {
+      payTotal,
+      incomeTotal,
+      balance: incomeTotal + payTotal,
+    }
+  }, [currentMonthList])
+  console.log(moneyTotal)
 
   return (
     <div className='monthlyBill'>
@@ -40,23 +54,23 @@ function Month() {
         <div className='header'>
           {/* 时间切换区域 */}
           <div className='date' onClick={() => setDateVisible(true)}>
-            <span className='text'>{currentDate} 月账单</span>
+            <span className='text'>{currentDate}月账单</span>
             {/* 根据弹窗打开的状态控制类名, 让箭头向上还是向下 */}
             <span className={classNames('arrow', dateVisible && 'expend')}></span>
           </div>
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className='item'>
-              <span className='money'>{100} 元</span>
+              <span className='money'>{moneyTotal.payTotal.toFixed(2)} 元</span>
               <span className='type'>支出</span>
             </div>
             <div className='item'>
-              <span className='money'>{200} 元</span>
+              <span className='money'>{moneyTotal.incomeTotal.toFixed(2)} 元</span>
               <span className='type'>收入</span>
             </div>
             <div className='item'>
-              <span className='money'>{300}</span>
-              <span className='type'>结余 元</span>
+              <span className='money'>{moneyTotal.balance.toFixed(2)} 元</span>
+              <span className='type'>结余</span>
             </div>
           </div>
           {/* 时间选择器 */}
@@ -72,11 +86,20 @@ function Month() {
           />
         </div>
         {/* 明细 */}
-        {/* <div>
-          { monthGroup[currentDate].map((item) => (
-            <div key={item.id}>{item.id}</div>
+        <div className='detailWrapper'>
+          { currentMonthList.map((item) => (
+            <div key={item.id} className='item'>
+              <div className='baseInfo'>
+                <span>时间: {item.date}</span>
+                <span>类型: {item.type === 'pay' ? '支出' : '收入'}</span>
+              </div>
+              <div className='moneyInfo'>
+                <span>金额: {item.money}元</span>
+                <span>备注: {item.userFor}</span>
+              </div>
+            </div>
           )) }
-        </div> */}
+        </div>
       </div>
     </div>
   )
