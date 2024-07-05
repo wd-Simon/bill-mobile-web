7a79225b7b7e3c00dbb72fbf1191c379eae20494
xingsensong <xingsensong@naradapower.com>
2024-07-05 13:21:27 +0800

    feat; 页面路由, 底部TabBar

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,28 +1,68 @@
-import { Outlet } from 'react-router-dom'
-import { Button } from 'antd-mobile'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { getBillList } from '@/store/modules/billStore'
+import { TabBar } from 'antd-mobile'
+import {
+  BillOutline,
+  AddSquareOutline,
+  CalculatorOutline,
+} from 'antd-mobile-icons'
+
+import './index.scss'
+
+const tabs = [
+  {
+    key: '/month',
+    title: '月度账单',
+    icon: <BillOutline />,
+  },
+  {
+    key: '/new',
+    title: '记账',
+    icon: <AddSquareOutline />,
+  },
+  {
+    key: '/year',
+    title: '年度账单',
+    icon: <CalculatorOutline />,
+  },
+]
 
 function Layout() {
+  // 请求列表数据
   const dispatch = useDispatch()
-
   useEffect(() => {
     dispatch(getBillList())
   }, [dispatch])
 
+  // 路由跳转逻辑
+  const navigate = useNavigate()
+  const setRouteActive = (path) => {
+    console.log(path)
+    navigate(path)
+  }
+
+  // 获取当前路由
+  const location = useLocation()
+  const { pathname } = location
+
   return (
-    <div>
-      <h3>我是Layout</h3>
-      {/* 测试全局生效样式 */}
-      <Button color='primary'>测试全局</Button>
+    <div className='layout'>
 
-      {/* 测试局部生效样式 */}
-      {/* <div className='puple'>
-        <Button color='primary'>测试局部</Button>
-      </div> */}
-
-      <Outlet></Outlet>
+      {/* 二级路由出口 */}
+      <div className='container'>
+        <Outlet></Outlet>
+      </div>
+      
+      <div className='footer'>
+        {/* 循环tab */}
+        <TabBar activeKey={pathname} onChange={setRouteActive}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   )
 }
--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,8 +1,84 @@
-import React from 'react'
+import { NavBar, DatePicker } from 'antd-mobile'
+import './index.scss'
+import { useMemo, useState } from 'react'
+import classNames from 'classnames'
+import dayjs from 'dayjs'
+import _ from 'lodash'
+import { useSelector } from 'react-redux'
 
 function Month() {
+  // 从redux中取数据并按月分组
+  const { billList }  = useSelector(state => state.bill)
+  // useMemo类似计算属性
+  const monthGroup = useMemo(() => {
+    // return出去计算后的值
+    return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY-MM') )
+  }, [billList])
+  console.log(monthGroup)
+
+  // 控制时间弹窗的打开和关闭
+  const [dateVisible, setDateVisible] = useState(false)
+  // 时间显示
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().format('YYYY-MM')
+  })
+  // 时间切换
+  const onDateConfirm = (date) => {
+    setDateVisible(false)
+    setCurrentDate(dayjs(date).format('YYYY-MM'))
+  }
+
+  console.log(monthGroup[currentDate])
+
   return (
-    <div>我是Month</div>
+    <div className='monthlyBill'>
+      <NavBar className='nav' backIcon={false}>
+        月度收支
+      </NavBar>
+      
+      <div className='content'>
+        <div className='header'>
+          {/* 时间切换区域 */}
+          <div className='date' onClick={() => setDateVisible(true)}>
+            <span className='text'>{currentDate} 月账单</span>
+            {/* 根据弹窗打开的状态控制类名, 让箭头向上还是向下 */}
+            <span className={classNames('arrow', dateVisible && 'expend')}></span>
+          </div>
+          {/* 统计区域 */}
+          <div className='twoLineOverview'>
+            <div className='item'>
+              <span className='money'>{100} 元</span>
+              <span className='type'>支出</span>
+            </div>
+            <div className='item'>
+              <span className='money'>{200} 元</span>
+              <span className='type'>收入</span>
+            </div>
+            <div className='item'>
+              <span className='money'>{300}</span>
+              <span className='type'>结余 元</span>
+            </div>
+          </div>
+          {/* 时间选择器 */}
+          <DatePicker
+            className='kaDate'
+            title="记账日期"
+            precision="month"
+            visible={dateVisible}
+            max={new Date()}
+            onCancel={() => setDateVisible(false)}
+            onClose={() => setDateVisible(false)}
+            onConfirm={val => onDateConfirm(val)}
+          />
+        </div>
+        {/* 明细 */}
+        {/* <div>
+          { monthGroup[currentDate].map((item) => (
+            <div key={item.id}>{item.id}</div>
+          )) }
+        </div> */}
+      </div>
+    </div>
   )
 }
 
